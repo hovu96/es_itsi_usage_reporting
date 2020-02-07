55e712aab61d82b21b56fe8c33010d21a9d2d1b7
hovu96 <hovu96>
2020-02-07 19:27:59 +0000

    Wait for events to appear in summary index after backfill

--- appserver/static/configure.js
+++ appserver/static/configure.js
@@ -181,53 +181,84 @@ require([
     });
 
     $("#setupButton").click(async function () {
+        const delay = ms => new Promise(res => setTimeout(res, ms));
         const progressIndicator = Utils.newLoadingIndicator({
             title: "Saving Settings ...",
             subtitle: "Please wait."
         });
         try {
+            console.log("setting summary index name...");
             await propertiesEndpoint.postAsync('macros/summary_index_name/definition', {
                 "value": $("splunk-text-input[name=\"summary_index_name\"]").attr('value')
             });
 
-            const summarySearch = await savedEndpoint.getAsync('searches/summarize_license_usage');
-
             const enableSummarySearch = $("splunk-radio-input[name=\"summary_indexing_enabled\"]").attr('value') == "1";
             if (enableSummarySearch) {
+                console.log("checking for summary index events...");
                 const results = await Utils.searchAsync(
                     "search index=`summary_index_name` search_name=\"summarize_license_usage\" | stats latest(_time) as last_summary | eval no_summaries_in_days = (now() - last_summary) / 60 / 60 / 24 | table no_summaries_in_days", {
                     "earliest_time": `-30@d`,
                     "latest_time": "now",
                 });
                 var doBackfillHours = 0;
-                if (results.rows > 0) {
+                if (results.rows.length > 0) {
+                    console.log("found events in summary index");
                     const noSummariesSinceDays = Math.round(results.rows[0][0] * 100) / 100;
                     if (noSummariesSinceDays > 0.09) {
+                        console.log("ask for backfill");
                         if (confirm(`No summaries found for last ${noSummariesSinceDays} days.\n\nDo you want to backfill the summary index now?`)) {
                             doBackfillHours = Math.round(noSummariesSinceDays * 24);
                         } else {
+                            console.log("user stopped");
                             return;
                         }
                     }
                 } else {
+                    console.log("no events in summary index found -> ask for backfill");
                     if (confirm(`No summaries found.\n\nDo you want to backfill the summary index now?`)) {
                         doBackfillHours = 24 * 30;
                     } else {
+                        console.log("user stopped");
                         return;
                     }
                 }
                 if (doBackfillHours > 0) {
+                    //TODO: check index exists
                     const backfillProgressIndicator = Utils.newLoadingIndicator({
                         title: "Performing Backfill ...",
                         subtitle: "Please wait."
                     });
                     try {
+                        console.log("performing backfill...");
                         await Utils.searchAsync("savedsearch summarize_license_usage", {
                             "earliest_time": `-${doBackfillHours}h@d`,
                             "latest_time": "@h",
                         });
                     } finally {
+                        console.log("performing done");
                         backfillProgressIndicator.hide();
+                    }
+                    console.log("wait for data to appear in summary index...");
+                    var found = false;
+                    for (var i = 0; i < 120; i++) {
+                        await delay(1000);
+                        console.log("looking for data...");
+                        const searchTestingForData = await Utils.searchAsync(
+                            "| datamodel License_Usage License_Usage search | head 1", {
+                            "earliest_time": `-30d`,
+                            "latest_time": "now",
+                        });
+                        if (searchTestingForData.rows.length > 0) {
+                            console.log("found some data");
+                            found = true;
+                            break;
+                        }
+                        console.log("no data found yet");
+                    }
+                    if (!found) {
+                        console.log("no data found after some time -> stop here");
+                        alert("Backfill did not work, because no license usage events could not be found in summary index.");
+                        return;
                     }
                 }
             }
@@ -262,6 +293,18 @@ require([
             await savedEndpoint.postAsync('searches/summarize_license_usage', {
                 "is_scheduled": enableSummarySearch ? 1 : 0
             });
+
+            console.log("searching for licence usage events using data model ...")
+            const datamodelTestSearchResults = await Utils.searchAsync(
+                "| datamodel License_Usage License_Usage search | head 1", {
+                "earliest_time": `-30d`,
+                "latest_time": "now",
+            });
+            console.log(datamodelTestSearchResults);
+            if (datamodelTestSearchResults.rows.length == 0) {
+                console.log("could not find usage events using data model -> warn user")
+                alert("Warning: No results in summary index. Please check index exists and contains events.");
+            }
 
             await propertiesEndpoint.postAsync('app/install/is_configured', {
                 "value": "1"
